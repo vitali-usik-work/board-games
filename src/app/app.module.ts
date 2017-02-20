e0f94f93e61195a0cc0ecdb8ab611ebffe649411
vitali-usik-work <usik.vitaly@gmail.com>
2017-02-20 17:20:34 +0300

    add containers/main-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { Main } from './containers';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    Main
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/containers/main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-container',
+  template: `
+    <div>
+      <main class="main">
+        content here
+      </main>
+    </div>`
+})
+export class Main {};
